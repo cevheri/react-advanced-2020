bd1f8812b10dd650d892b58fe38e61368bba52c9
Cevheri <cevheribozoglan@gmail.com>
2021-09-27 18:30:36 +0300

    useState Array and object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+// first way
+// const UseStateObject = () => {
+//   const [person, setPerson] = useState({
+//     name:"peter",
+//     age:24,
+//     message: "random message"
+//   });
+
+//   const changeMessage = ()=>{
+//     setPerson({...person, message:"message was changed"})
+//   }
+
+//   return (
+//     <>
+//     <h3>{person.name}</h3>
+//     <h3>{person.age}</h3>
+//     <h3>{person.message}</h3>
+//     <button className="btn" onClick={changeMessage}>change message</button>
+//     </>
+//   );
+// };
+///////////////////////////
+//second way
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    setMessage("message was changed");
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
