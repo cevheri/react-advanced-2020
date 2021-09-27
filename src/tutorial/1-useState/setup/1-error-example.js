fb8063734f316c0335ead5d02eb43aec84f241b5
Cevheri <cevheribozoglan@gmail.com>
2021-09-27 14:43:16 +0300

    basic useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+  let title = "random title";
+  const handleClick=()=>{
+    title="new title value";
+    console.log(title);
+  }
+  return (
+    <React.Fragment>
+      <h2>
+        {title}
+      </h2>
+      <button type="button" onClick={handleClick} className="btn">change title</button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("random title");
+  //console.log(useState("random title"));
+  // const val = useState("random title")[0];
+  // const valEvent = useState("random title")[1];
+  // console.log(val);
+  // console.log(valEvent);
+
+  const clickHandler = () => {
+    if (text === "random title") {
+      setText("hello react");
+    } else {
+      setText("random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={clickHandler}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
